b1e88136f759c57609dd4b3544d1ccc3db1ee71f
nuhaaelf <98462099+nuhaaelf@users.noreply.github.com>
2023-11-28 16:43:10 +0100

    Update EtudiantApplication.java

--- src/main/java/com/n7iila/EtudiantApplication.java
+++ src/main/java/com/n7iila/EtudiantApplication.java
@@ -15,18 +15,39 @@ public class EtudiantApplication {
 	@Bean
 	CommandLineRunner start(EtudiantRepository etudiantRepository)
 	{return args -> {
-		for(int i=0;i<10;i++) {
-			Etudiant c= Etudiant.builder()
+			Etudiant c0= Etudiant.builder()
 				      .idEtudiant((int) Math.random())
+				      .nom("Mgarred")
+				      .prenom("Wijdane")
+				      .email("wijdanox@gmail.com")
+				      .build();
+			etudiantRepository.save(c0); 
+			
+			
+			Etudiant c1= Etudiant.builder()
+				      .idEtudiant((int) Math.random())
+				      .nom("El fid")
+				      .prenom("Nouhaila")
 				      .email("elfidnouhaila7@gmail.com")
 				      .build();
-			etudiantRepository.save(c); 
-		}
+			etudiantRepository.save(c1); 
+			
+			
+			Etudiant c2= Etudiant.builder()
+				      .idEtudiant((int) Math.random())
+				      .nom("Jundi")
+				      .prenom("Ihssane")
+				      .email("ihssanox@gmail.com")
+				      .build();
+			etudiantRepository.save(c2); 
+		
 		List<Etudiant> listes=etudiantRepository.findAll();
 	
 		  
 		  for(Etudiant cp:listes) {
 		  System.out.println(cp.getIdEtudiant());
+		  System.out.println(cp.getPrenom());
+		  System.out.println(cp.getNom());
 		  System.out.println(cp.getEmail());}
 
 };
